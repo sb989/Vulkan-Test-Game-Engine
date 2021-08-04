081731d9d20879acf4118c8c98bdb4915a2228bb
Suri <surindraboodhoo@gmail.com>
2021-08-04 19:43:42 -0400

    pauses while minimized

--- vtge.cpp
+++ vtge.cpp
@@ -135,6 +135,13 @@ class TestEngine{
         }
 
         void recreateSwapChain(){
+            int width = 0, height = 0;
+            glfwGetFramebufferSize(window, &width, &height);
+            while (width == 0 || height == 0){
+                glfwGetFramebufferSize(window, &width, &height);
+                glfwWaitEvents();
+            }
+            
             vkDeviceWaitIdle(device);
 
             cleanupSwapChain();
